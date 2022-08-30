471eb12dd710f94190d9d8069baf57c9a3016822
AdnSmile <nessafitri16@gmail.com>
2022-08-30 09:14:15 +0700

    Membuat 3 activity yaitu StartPage, RegisterPage dan LoginPage

---
+++ app/src/main/java/com/tubes/salon_kelompok_4/StartPage.kt
@@ -0,0 +1,11 @@
+package com.tubes.salon_kelompok_4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_page)
+    }
+}
